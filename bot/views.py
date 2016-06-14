5852eea4a413ea83a89b39c505932e526d9c36e7
BuildTools <unconfigured@null.spigotmc.org>
2016-06-15 07:37:44 +0800

    Fix spell error

--- bot/views.py
+++ bot/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def recvreq(request):
-    body = request.body.decode('urf-8')
+    body = request.body.decode('utf-8')
     print('\nLine Request:\n%s\n' % body)
     return HttpResponse(u"<h1>Hello World</h1>")
