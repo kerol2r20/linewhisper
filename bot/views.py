a0f324655cc80bdc74e4658d4ae802ede263ef48
BuildTools <unconfigured@null.spigotmc.org>
2016-06-15 20:29:00 +0800

    Test

--- bot/views.py
+++ bot/views.py
@@ -5,7 +5,7 @@ import json
 import re
 
 
-sendHeader = {'Content-Type':'application/json; charser=UTF-8', 'X-Line-ChannelID':'1467107178', 'X-Line-ChannelSecret':'ee260eb823d4a0c71c54e3a401539acf','X-Line-Trusted-User-With-ACL','u24ba734c4e2a0b5f06be24b80cf479b0'}
+sendHeader = {'Content-Type':'application/json; charser=UTF-8', 'X-Line-ChannelID':'1467107178', 'X-Line-ChannelSecret':'ee260eb823d4a0c71c54e3a401539acf','X-Line-Trusted-User-With-ACL':'u24ba734c4e2a0b5f06be24b80cf479b0'}
 
 def sendMessageBuild(target,content):
     mesg = {'to':target,'toChannel':'1383378250','eventType':'138311608800106203','content':{'contentType':'1','toType':'1','text':content}}
