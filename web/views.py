5c7e9ab7c789371705c9be4cdcc820d69c018add
ccuchengwei <yy316004@gmail.com>
2016-06-15 22:12:55 +0800

    renew

--- web/views.py
+++ web/views.py
@@ -7,16 +7,41 @@ import uuid
 
 
 def index(request):
-	
-	return render(request,'web/index.html')
+	if request.session.get('Email'):
+		return render(request,'web/mainpage.html')
+	else:
+		return render(request,'web/index.html')
+def signin(request):
+	if	request.POST:
+		return render(request,'web/index.html')
+
+	else:
+		return render(request,'web/index.html')
 def signup(request):
 	if	request.POST:
 		utoken = uuid.uuid5(uuid.NAMESPACE_URL, request.POST['upEmail'])
-		
+		request.session['Email'] = request.POST['upEmail']
 		Account.objects.get_or_create(nickname = request.POST['upNickname'],token = str(utoken), email = request.POST['upEmail'], password = request.POST['upPassword'], mid = '')
+		return HttpResponseRedirect(reverse('index'))
 		
-		return HttpResponse(Account.objects.get(nickname = request.POST['upNickname']))
 	else:
 		return HttpResponseRedirect(reverse('index'))
+def logout(request):
+	del request.session['Email']
+	return HttpResponseRedirect(reverse('index'))
+def checkemail(request):
+	email = request.POST['email']
+	try:
+		Account.objects.get(email=email)
+		return HttpResponse("error")
+	except :
+		return HttpResponse("ok")
+def checknickname(request):
+	nickname = request.POST['nickname']
+	try:
+		Account.objects.get(nickname = nickname)
+		return HttpResponse("error")
+	except :
+		return HttpResponse("ok")
 
 # Create your views here.
