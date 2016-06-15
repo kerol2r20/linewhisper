412bcef8ac8ba2d139bde7c3c3ae48e7b720d0ae
BuildTools <unconfigured@null.spigotmc.org>
2016-06-15 15:42:10 +0800

    Add model

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    nickname = models.charField(max_length=200)
+    token = models.charField(max_length=200)
+    email = models.EmailField(max_length=200)
+    password = models.charField(max_length=200)
+    mid = models.charField(max_length=200)
