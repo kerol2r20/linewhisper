ae07827817a345b5ccec6fbd36483d2af14ad3b0
BuildTools <unconfigured@null.spigotmc.org>
2016-06-15 17:43:44 +0800

    Add __str__ to Account model

--- bot/views.py
+++ bot/views.py
@@ -3,10 +3,12 @@ from django.http import HttpResponse
 from bot.models import Account
 import json
 
-# Create your views here.
+
 def recvreq(request):
     body = request.body.decode('utf-8')
     rawJson = json.loads(body)
     for result in rawJson['result']:
         print('Recv: {}'.format(result['content']['text']))
+    fufu = Account.objects.get(name='Fufu')
+    print('Test: fufu.email={}'.format(fufu['email']))
     return HttpResponse(u"<h1>Hello World</h1>")
