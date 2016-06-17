c1d33e156d85bff1a43325153fb433aa5f01aeea
ccuchengwei <yy316004@gmail.com>
2016-06-17 16:53:27 +0800

    renew

--- bot/views.py
+++ bot/views.py
@@ -6,15 +6,19 @@ import json
 import re
 import requests
 import random
-
-
+helpmsg = '註冊 /new token\n密語!name message\ndice /dice'
+sendHeader = {'Content-type':'application/json; charset=UTF-8','X-Line-ChannelID':'1467107178','X-Line-ChannelSecret':'ee260eb823d4a0c71c54e3a401539acf','X-Line-Trusted-User-With-ACL':'u24ba734c4e2a0b5f06be24b80cf479b0'}
+url = 'https://trialbot-api.line.me/v1/events'
 def sendMessageBuild(target,content):
     mesg = {'to':target,'toChannel':'1383378250','eventType':'138311608800106203','content':{'contentType':'1','toType':'1','text':content}}
     return mesg
-
+def sendMessage(target,content):
+    mesg = {'to':target,'toChannel':'1383378250','eventType':'138311608800106203','content':{'contentType':'1','toType':'1','text':content}}
+    Msg = json.dumps(mesg)
+    req = requests.post(url,data=Msg,headers=sendHeader)
+    return
 def recvreq(request):
-    sendHeader = {'Content-type':'application/json; charset=UTF-8','X-Line-ChannelID':'1467107178','X-Line-ChannelSecret':'ee260eb823d4a0c71c54e3a401539acf','X-Line-Trusted-User-With-ACL':'u24ba734c4e2a0b5f06be24b80cf479b0'}
-    url = 'https://trialbot-api.line.me/v1/events'
+
     body = request.body.decode('utf-8')
     rawJson = json.loads(body)
     for result in rawJson['result']:
@@ -31,10 +35,7 @@ def recvreq(request):
                 if text.startswith("/"):
                     command = re.match('^/(\w+)',text).group(1)
                     if command == 'help':
-                        helpmsg = '註冊 /new token\n密語!name message\ndice /dice'
-                        MsgBuild = sendMessageBuild([senderMID],helpmsg)
-                        Msg = json.dumps(MsgBuild)
-                        req = requests.post(url,data=Msg,headers=sendHeader)
+                        sendMessage([senderMID],helpmsg)
                         continue
                     elif command == 'dice':
                         diceresult = random.randrange(1,7)
@@ -104,7 +105,7 @@ def recvreq(request):
                                 req = requests.post(url,data=Msg,headers=sendHeader)
                                 continue
                     elif command == 'help':
-                        helpmsg = '註冊 /new token\n密語!name message\ndice /dice'
+
                         MsgBuild = sendMessageBuild([senderMID],helpmsg)
                         Msg = json.dumps(MsgBuild)
                         req = requests.post(url,data=Msg,headers=sendHeader)
